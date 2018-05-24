420f2e96cf69d4c790e9576b83aeb7dea45c497c
ailindoyle <caitlin.doyle@mobile-life.com>
2018-05-24 13:28:30 +0100

    api functional, some tech debt

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded())
 //mongo stuff
 const url = 'mongodb://localhost:27017/toDo'
 
-
 app.get('/api/tasks', function(req, res) {
 
     MongoClient.connect(url, function(err, client) {
@@ -26,7 +25,6 @@ app.get('/api/tasks', function(req, res) {
 
 })
 
-
 app.post('/api/task', function(req, res) {
 
     MongoClient.connect(url, function(err, client) {
@@ -38,17 +36,12 @@ app.post('/api/task', function(req, res) {
 
     let insertTaskToDb = function(db, newTask) {
         let tasksCollection = db.collection('tasks')
-        tasksCollection.insertOne({ task : newTask }, function(err, result) {
-            if (err) {
-                res.json({"success" : "false", "msg" : "Task has not saved", "data" : []})
-            } else {
-                res.json({"success" : "true", "msg" : "Task has saved successfully", "data" : []})
-            }
+        tasksCollection.insertOne({ "task" : newTask }, function(err, result) {
+            res.json({"success" : "true", "msg" : "Task has saved successfully", "data" : []})
         })
     }
 
 })
-
 
 app.put('/api/task', function(req, res) {
 
@@ -79,7 +72,6 @@ app.put('/api/task', function(req, res) {
 
 })
 
-
 app.delete('/api/task', function(req, res) {
 
     MongoClient.connect(url, function(err, client) {
@@ -106,56 +98,5 @@ app.delete('/api/task', function(req, res) {
     }
 
 })
-
-
-
-
-// app.put('/task/:id?complete=:completeValue', function(req, res) {
-//     res.json({ user: 'bob' })
-// })
-
-
-//EXAMPLE MONGO CODE
-
-// //insert to db
-// let insertIntoDb = function(db) {
-//     let tasksCollection = db.collection('tasks')
-//     tasksCollection.insertOne({
-//         "task name" : "Grocery shopping"
-//     }, function(err, result) {
-//         console.log("Inserted new task into the tasks collection")
-//     })
-// }
-//
-// //get data from db
-// let getDataFromDb = function(db) {
-//     let tasksCollection = db.collection('tasks')
-//     tasksCollection.find({}).toArray(function (err, docs) {
-//         console.log("Found the following records:")
-//         console.log(docs)
-//     })
-// }
-//
-// //update data in db
-// let updateDataInDb = function(db) {
-//     let tasksCollection = db.collection('tasks')
-//     tasksCollection.updateOne({"task name" : "Grocery shopping"},
-//         { $set: {"completed" : 1} }, function (err, result) {
-//             console.log("Updated the document with grocery shopping now being completed")
-//         })
-// }
-//
-// //remove data from db
-// let removeDataInDb = function(db) {
-//     let tasksCollection = db.collection('tasks')
-//     tasksCollection.deleteOne({"task name" : "Grocery shopping"}, function (err, result) {
-//         console.log("Removed document with field task name: grocery shopping")
-//     })
-// }
-
-
-//routes
-
-
 
 app.listen(3000, () => console.log('Listening on port 3000'))
